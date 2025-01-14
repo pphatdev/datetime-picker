224b4480991d2b323c3ee450a882f89e9ba5c4c8
PPhatDev <leatsophat02@gmail.com>
2025-01-14 13:37:07 +0700

    ⚙️: Replace Config Deployement

---
+++ replace.py
@@ -0,0 +1,18 @@
+import os
+
+def replace_assets(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    content = content.replace('/index', './index')
+
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+if __name__ == "__main__":
+    dist_path = 'dist/index.html'  # Update this path if needed
+    if os.path.exists(dist_path):
+        replace_assets(dist_path)
+        print(f'Replaced "/assets" with "./assets" in {dist_path}')
+    else:
+        print(f'File {dist_path} does not exist')
